6724fd81b358ed3777d37dcabc09768355b96265
Abraham Justin <162578329+abrahamjust@users.noreply.github.com>
2025-05-24 09:07:39 +0000

    removed the unneccessary check and replaced it with e.stopPropogation()

--- src/handleDialog.js
+++ src/handleDialog.js
@@ -23,16 +23,16 @@ function setupProjectDialogs() {
 
     // Open on clicking the Project div (excluding clicks on the dialog or button)
     projectDiv.addEventListener("click", (event) => {
-      // Prevent re-triggering if click is inside the dialog or button
-      // since the dialog is inside the div, if i dont have the check, it is like clicking on the div, so it opens again (the event bubbles up)
-      // basically stops the event (opening event) from happening inside the dialog or on the close button
-      if (event.target.tagName.toLowerCase() === "button" || dialog.contains(event.target)) return;
-
       dialog.showModal();
     });
 
-    closeButton.addEventListener("click", () => {
-      dialog.close();
+    closeButton.addEventListener("click", (event) => {
+        // Prevent re-triggering if click is inside the dialog or button
+        // since the dialog is inside the div, if i dont have the check, it is like clicking on the div, so it opens again (the event bubbles up)
+        // basically stops the event (opening event) from happening inside the dialog or on the close button
+        // prevents event bubbling
+        event.stopPropagation();
+        dialog.close();
     });
   }
 }
